189665a9aed609328a5eb41d22cde71803212758
Nadiia Bulmak <nadgaval@gmail.com>
2025-08-01 14:00:53 +0300

    init

--- src/modules/lessons/pages/lessons.page.tsx
+++ src/modules/lessons/pages/lessons.page.tsx
@@ -1,11 +1,28 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 type LessonsPageProps = null;
 
+type lessonsType = 'Курси' | 'Факультативи' | 'Гуртки' | 'ГПД' | 'Індивідуальні';
+
 export const LessonsPage: FC<LessonsPageProps> = () => {
+  const [lessonsFilter, setLessonsFilter] = useState<lessonsType>('Курси');
+
+  const filterTypes: lessonsType[] = ['Курси', 'Факультативи', 'Гуртки', 'ГПД', 'Індивідуальні'];
+
   return (
     <main className='lessons-page'>
       <h1>Lessons Page</h1>
+      <div className='filter-panel'>
+        {filterTypes.map((type) => (
+          <button
+            key={type}
+            className={lessonsFilter === type ? 'active' : ''}
+            onClick={() => setLessonsFilter(type)}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
     </main>
   );
 };
